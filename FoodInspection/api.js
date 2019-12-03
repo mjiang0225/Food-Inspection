e425413d13b68d0a3bf5fe43193c5aaeae6bcbac
mjiang0225 <54859874+mjiang0225@users.noreply.github.com>
2019-12-03 13:19:21 -0500

    Add files via upload

--- FoodInspection/api.js
+++ FoodInspection/api.js
@@ -36,12 +36,23 @@
                     var arr = [];
                     for (var i = 0, len = result.length; i < len; i++) {
                             if(name != '') {
-                                if(name == result[i].name && category == result[i].category && zip == result[i].zip) {
+                                if(name == result[i].name || category == result[i].category || zip == result[i].zip) {
                                     str += '<tr><td>'+ result[i].name +'</td><td>'+ result[i].category +'</td><td>'+ result[i].inspection_date +'</td><td>'+ result[i].inspection_results +'</td><td>'+ result[i].city +'</td><td>'+ result[i].zip +'</td><td>'+ result[i].address_line_1 +'</td><td>'+ result[i].address_line_2 +'</td><td>'+ result[i].inspection_type +'</td></tr>';
                                 }
-                            } else {
-                                str += '<tr><td>'+ result[i].name +'</td><td>'+ result[i].category +'</td><td>'+ result[i].inspection_date +'</td><td>'+ result[i].inspection_results +'</td><td>'+ result[i].city +'</td><td>'+ result[i].zip +'</td><td>'+ result[i].address_line_1 +'</td><td>'+ result[i].address_line_2 +'</td><td>'+ result[i].inspection_type +'</td></tr>';
                             }
+                            else if (category != '') {
+                                if(name == result[i].name || category == result[i].category || zip == result[i].zip) {
+                                    str += '<tr><td>'+ result[i].name +'</td><td>'+ result[i].category +'</td><td>'+ result[i].inspection_date +'</td><td>'+ result[i].inspection_results +'</td><td>'+ result[i].city +'</td><td>'+ result[i].zip +'</td><td>'+ result[i].address_line_1 +'</td><td>'+ result[i].address_line_2 +'</td><td>'+ result[i].inspection_type +'</td></tr>';
+                                }
+                            }
+                            else if (zip != '') {
+                                if(name == result[i].name || category == result[i].category || zip == result[i].zip) {
+                                    str += '<tr><td>'+ result[i].name +'</td><td>'+ result[i].category +'</td><td>'+ result[i].inspection_date +'</td><td>'+ result[i].inspection_results +'</td><td>'+ result[i].city +'</td><td>'+ result[i].zip +'</td><td>'+ result[i].address_line_1 +'</td><td>'+ result[i].address_line_2 +'</td><td>'+ result[i].inspection_type +'</td></tr>';
+                                }
+                            } 
+                            /**else {
+                                str += '<tr><td>'+ result[i].name +'</td><td>'+ result[i].category +'</td><td>'+ result[i].inspection_date +'</td><td>'+ result[i].inspection_results +'</td><td>'+ result[i].city +'</td><td>'+ result[i].zip +'</td><td>'+ result[i].address_line_1 +'</td><td>'+ result[i].address_line_2 +'</td><td>'+ result[i].inspection_type +'</td></tr>';
+                            } **/ 
                             
                          
                          
@@ -61,18 +72,7 @@
                 var name = document.getElementById('name').value;
                 var category = document.getElementById('category').value;
                 var zip = document.getElementById('zip').value;
-                if(name == '') {
-                    alert('Name is required');
-                    return;
-                }
-                if(category == '') {
-                    alert('Category is required');
-                    return;
-                }
-                if(zip == '') {
-                    alert('Zip is required');
-                    return;
-                }
+               
             }
             document.getElementById('table').style.display = 'block';
             //clear the last results
